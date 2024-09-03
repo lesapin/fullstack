bcde2ee655099952336d3ac9189313b5fcc0a432
Roope Hurttia <hurtsi@iki.fi>
2024-09-03 13:38:08 +0300

    part4 update

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blog')
 
 const cors = require('cors')
+
+mongoose.connect(config.MONGODB)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(err => {
+    logger.error('error:', err.message)
+  })
 
 app.use(cors())
 app.use(express.static('dist'))
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -7,15 +7,4 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-const mongoUrl = process.env.MONGODB
-console.log('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(err => {
-    console.log('error:', err.message)
-  })
-
 module.exports =  mongoose.model('Blog', blogSchema)
